52a96a081bb314099bf4683c1a015b67cc81582f
Atharvjainn <jainatharv20@gmail.com>
2025-12-24 19:44:55 +0530

    added logout

--- backend/src/controllers/auth-controller.js
+++ backend/src/controllers/auth-controller.js
@@ -81,3 +81,18 @@ export const login = async(req,res) => {
         })
     }
 }
+
+
+export const logout = async (req,res) => {
+    try {
+        res.cookie("jwt","",{maxAge : 0})
+        return res.status(200).json({
+            message : "User logged out successfully!"
+        })
+    } catch (error) {
+        console.log("Error in logout controller!",error.message);
+        return res.status(500).json({
+            message : "Internal server error"
+        })
+    }
+}
--- backend/src/routes/auth-routes.js
+++ backend/src/routes/auth-routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { signup,login } from "../controllers/auth-controller.js";
+import { signup,login,logout } from "../controllers/auth-controller.js";
 import { login_middlewares, signup_middlewares } from "../middlewares/auth-middlewares.js";
 const router = express.Router();
 
 router.post("/login",login_middlewares,login);
 router.post("/signup",signup_middlewares,signup);
-// router.post("/logout",logout);
+router.post("/logout",logout);
 
 export default router;
