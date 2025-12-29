2467d48c9f270d9351c495942f877e20cabe0f4f
Atharvjainn <jainatharv20@gmail.com>
2025-12-30 02:13:44 +0530

    jwt fixed

--- backend/src/utils/helperfns.js
+++ backend/src/utils/helperfns.js
@@ -18,7 +18,7 @@ export const generateToken = async(userId,res) => {
         maxAge : 7*24*60*60*1000,
         httpOnly : true,
         sameSite : "strict",
-        secure : NODE_ENV === "development" ? false : true,
+        secure : true,
     })
     return token
 }
